eb13f3f84f9aef15c39f7adbf2e5a293c3b5e260
Francesco Nicoletta Puzzillo <francesconicoletta@nomnp.com>
2025-03-25 08:49:12 +0100

    nvim: add neovim config

---
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,12 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '<leader>e', ':Neotree toggle<CR>', desc = 'NeoTree toggle', silent = true },
+  },
+}
